48a1b3e6b486fee6b50f8e8f363eb96aeb561c16
jean850727 <k.h.cj.850727@hotmail.com>
2024-06-07 12:40:51 -0600

    Add files via upload

---
+++ alura-bank-main/alura-bank-main/js/validar-edad.js
@@ -0,0 +1,13 @@
+export default function esMayorDeEdad(campo){
+    const fechaDeNacimiento= new Date(campo.value);
+    if(!validarEdad(fechaDeNacimiento)){
+        campo.setCustomValidity("Necesitas se mayor de edad")
+    }
+}
+
+function validarEdad(fecha){
+    const fechaActual= new Date();
+    const fechaMas18= new Date(fecha.getUTCFullYear()+18, fecha.getUTCMonth(), fecha.getUTCDate());
+
+    return fechaActual >= fechaMas18;
+}
